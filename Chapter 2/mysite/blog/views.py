79b4665e6775434512e08d32325efebecc9907bc
Marco <marcoserena96@gmail.com>
2022-10-07 01:54:30 +0200

    End of Chapter 2

--- Chapter 2/mysite/blog/views.py
+++ Chapter 2/mysite/blog/views.py
@@ -4,23 +4,32 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 2)   # 2 posts in each page
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         # If the page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 
-#                     'blog/post/list.html', 
-#                     {'page': page,
-#                     'posts': posts})
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 2)   # 2 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If the page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 
+                    'blog/post/list.html', 
+                    {'page': page,
+                    'posts': posts,
+                    'tag': tag})
 
 
 # This replace, in a more compact and easy way to remember, all the code written above
@@ -42,6 +51,11 @@ def post_detail(request, year, month, day, post):
 
     new_comment = None
 
+    #List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     if request.method == 'POST':
         # A comment was posted
         comment_form = CommentForm(data=request.POST)
@@ -59,7 +73,8 @@ def post_detail(request, year, month, day, post):
                     {'post': post,
                     'comments': comments,
                     'new_comment': new_comment,
-                    'comment_form': comment_form})
+                    'comment_form': comment_form,
+                    'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
